c661088d69440f07d030bf3675b24b6e3e4a9dbb
ZhengZicong <578948046@qq.com>
2018-03-13 10:03:45 +0800

    update

---
+++ Libraries/RANSUROTTO.BLOG.Core/Caching/ICacheManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RANSUROTTO.BLOG.Core.Caching
+{
+    public interface ICacheManager
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        T Get<T>(string key);
+
+        void Set(string key, object data, int cacheTime);
+
+        bool IsSet(string key);
+
+        void Remove(string key);
+
+        void RemoveByPattern(string pattern);
+
+        void Clear();
+
+    }
+}
