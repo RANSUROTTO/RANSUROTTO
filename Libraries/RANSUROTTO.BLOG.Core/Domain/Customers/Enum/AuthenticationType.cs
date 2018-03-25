d6fe5967f5e484efccc5e87688f7bcad16973542
ZhengZicong <578948046@qq.com>
2018-03-25 23:40:27 +0800

    update

---
+++ Libraries/RANSUROTTO.BLOG.Core/Domain/Customers/Enum/AuthenticationType.cs
@@ -0,0 +1,26 @@
+﻿namespace RANSUROTTO.BLOG.Core.Domain.Customers.Enum
+{
+
+    /// <summary>
+    /// 登录类型
+    /// </summary>
+    public enum AuthenticationType
+    {
+
+        /// <summary>
+        /// 仅可以使用用户名进行登录
+        /// </summary>
+        Username,
+
+        /// <summary>
+        /// 仅可以使用Email进行登录
+        /// </summary>
+        Email,
+
+        /// <summary>
+        /// 用户名及Email均可以用作登录
+        /// </summary>
+        UsernameOrEmail
+
+    }
+}
