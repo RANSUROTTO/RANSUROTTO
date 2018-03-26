0a5778ffbe5f4c677c2eb0207e45791eb2db8e0a
ZhengZicong <578948046@qq.com>
2018-03-26 11:13:28 +0800

    update

--- Libraries/RANSUROTTO.BLOG.Core/Domain/Customers/Setting/CustomerSettings.cs
+++ Libraries/RANSUROTTO.BLOG.Core/Domain/Customers/Setting/CustomerSettings.cs
@@ -1,11 +1,15 @@
 ﻿using RANSUROTTO.BLOG.Core.Configuration;
+using RANSUROTTO.BLOG.Core.Domain.Customers.Enum;
 
 namespace RANSUROTTO.BLOG.Core.Domain.Customers.Setting
 {
     public class CustomerSettings : ISettings
     {
 
-
+        /// <summary>
+        /// 获取或设置当前使用的登录类型
+        /// </summary>
+        public AuthenticationType CurrentAuthenticationType { get; set; }
 
 
     }
