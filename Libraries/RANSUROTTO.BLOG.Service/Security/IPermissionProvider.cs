860b47da034ce5164316a879141ec1c3bc23e506
ZhengZicong <578948046@qq.com>
2018-04-13 08:33:24 +0800

    update

---
+++ Libraries/RANSUROTTO.BLOG.Service/Security/IPermissionProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using RANSUROTTO.BLOG.Core.Domain.Security;
+
+namespace RANSUROTTO.BLOG.Service.Security
+{
+    /// <summary>
+    /// 权限提供商
+    /// </summary>
+    public interface IPermissionProvider
+    {
+
+        IEnumerable<PermissionRecord> GetPermissions();
+
+
+
+    }
+}
---
+++ Libraries/RANSUROTTO.BLOG.Service/Security/IPermissionService.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using RANSUROTTO.BLOG.Core.Domain.Customers;
+using RANSUROTTO.BLOG.Core.Domain.Security;
+
+namespace RANSUROTTO.BLOG.Service.Security
+{
+    /// <summary>
+    /// 权限业务层接口
+    /// </summary>
+    public interface IPermissionService
+    {
+
+        PermissionRecord GetPermissionRecordById(int permissionId);
+
+        PermissionRecord GetPermissionRecordBySystemName(string systemName);
+
+        IList<PermissionRecord> GetAllPermissionRecords();
+
+        void InsertPermissionRecord(PermissionRecord permission);
+
+        void UpdatePermissionRecord(PermissionRecord permission);
+
+        void InstallPermissions(IPermissionProvider permissionProvider);
+
+        void UninstallPermissions(IPermissionProvider permissionProvider);
+
+        bool Authorize(PermissionRecord permission);
+
+        bool Authorize(PermissionRecord permission, Customer customer);
+
+        bool Authorize(string permissionRecordSystemName);
+
+        bool Authorize(string permissionRecordSystemName, Customer customer);
+
+    }
+}
