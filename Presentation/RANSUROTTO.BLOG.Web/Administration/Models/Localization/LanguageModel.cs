af15bd820c5b1cb16c10eecb02adca4b6840a45c
ZhengZicong <578948046@qq.com>
2018-06-12 22:04:11 +0800

    修改初始化语言资源文件

--- Presentation/RANSUROTTO.BLOG.Web/Administration/Models/Localization/LanguageModel.cs
+++ Presentation/RANSUROTTO.BLOG.Web/Administration/Models/Localization/LanguageModel.cs
@@ -9,25 +9,25 @@ namespace RANSUROTTO.BLOG.Admin.Models.Localization
         /// <summary>
         /// 获取或设置名称
         /// </summary>
-        [ResourceDisplayName("Admin.Configuration.Language.Fields.Name")]
+        [ResourceDisplayName("Admin.Configuration.Languages.Fields.Name")]
         public string Name { get; set; }
 
         /// <summary>
         /// 获取或设置语言文化
         /// </summary>
-        [ResourceDisplayName("Admin.Configuration.Language.Fields.LanguageCulture")]
+        [ResourceDisplayName("Admin.Configuration.Languages.Fields.LanguageCulture")]
         public string LanguageCulture { get; set; }
 
         /// <summary>
         /// 获取或设置唯一的SEO代码
         /// </summary>
-        [ResourceDisplayName("Admin.Configuration.Language.Fields.UniqueSeoCode")]
+        [ResourceDisplayName("Admin.Configuration.Languages.Fields.UniqueSeoCode")]
         public string UniqueSeoCode { get; set; }
 
         /// <summary>
         /// 获取或设置显示顺序
         /// </summary>
-        [ResourceDisplayName("Admin.Configuration.Language.Fields.DisplayOrder")]
+        [ResourceDisplayName("Admin.Configuration.Languages.Fields.DisplayOrder")]
         public int DisplayOrder { get; set; }
 
         /// <summary>
@@ -39,13 +39,13 @@ namespace RANSUROTTO.BLOG.Admin.Models.Localization
         /// <summary>
         /// 获取或设置该语言是否支持“从右到左”显示方式
         /// </summary>
-        [ResourceDisplayName("Admin.Configuration.Language.Fields.Rtl")]
+        [ResourceDisplayName("Admin.Configuration.Languages.Fields.Rtl")]
         public bool Rtl { get; set; }
 
         /// <summary>
         /// 获取或设置该语言是否已发布
         /// </summary>
-        [ResourceDisplayName("Admin.Configuration.Language.Fields.Published")]
+        [ResourceDisplayName("Admin.Configuration.Languages.Fields.Published")]
         public bool Published { get; set; }
 
         // search
